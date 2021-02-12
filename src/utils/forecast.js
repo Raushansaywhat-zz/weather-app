84aa3b5747d44a08faef0ad0754646ad2191c539
Raushan Kumar <raushansaywhat@gmail.com>
2021-02-12 15:54:35 +0530

    Initial Commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,27 @@
+const request = require("request");
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=fb7af85042a1505d1cd86e3f86ceedbe&query=" +
+    latitude +
+    "," +
+    longitude +
+    "&units=f";
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        body.current.weather_descriptions[0] +
+          ". It is currently " +
+          body.current.temperature +
+          " degress out."
+      );
+    }
+  });
+};
+
+module.exports = forecast;
